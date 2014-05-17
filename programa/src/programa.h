0c97c0d2b92248f1cad2a43575293656acce32aa
hernan-erasmo <socialmetrix@gmail.com>
2014-05-17 12:39:05 -0300

    Se extrajo la definición de paquetes a un archivo .h aparte en /utils

--- programa/src/programa.h
+++ programa/src/programa.h
@@ -1,9 +1,4 @@
-typedef struct paquete_programa {
-	char id;
-	char *mensaje;
-	uint32_t sizeMensaje;
-	uint32_t tamanio_total;
-} t_paquete_programa;
+#include "../../utils/comunicacion.h"
 
 int checkArgs(int args);
 int crearLogger(t_log **logger);
---
+++ utils/comunicacion.h
@@ -0,0 +1,6 @@
+typedef struct paquete_programa {
+	char id;
+	char *mensaje;
+	uint32_t sizeMensaje;
+	uint32_t tamanio_total;
+} t_paquete_programa;
