8b1bce041d67fff788a9579adb819fff78c10fa8
hernan-erasmo <socialmetrix@gmail.com>
2014-06-10 18:26:15 -0300

    Se creó la interfaz para que el plp pueda utilizar el comando enviar_bytes de la UMV

--- kernel/src/plp.c
+++ kernel/src/plp.c
@@ -304,7 +304,12 @@ uint32_t crear_segmento_codigo(int socket_umv, t_pcb *pcb, t_paquete_programa *p
 		return resultado;
 	}
 
-	//escribir datos acá!
+	//enviar_bytes(base,offset,tamanio,buffer)
+
+	if(solicitar_enviar_bytes(socket_umv, pcb->seg_cod, 0, strlen(paquete->mensaje), paquete->mensaje, logger) == 0){
+		log_error(logger, "[PLP] La UMV no permitió escribir el código en el segmento de código del programa.");
+		return resultado;
+	}
 
 	resultado = 1;
 	return resultado;
@@ -373,6 +378,87 @@ uint32_t solicitar_crear_segmento(int socket_umv, uint32_t id_programa, uint32_t
 	free(respuesta.mensaje);
 
 	return valorRetorno;
+}
+
+uint32_t solicitar_enviar_bytes(int socket_umv, uint32_t base, uint32_t offset, int tamanio, char *buffer, t_log *logger)
+{
+	uint32_t bEnv = 0;
+	char *orden = codificar_enviar_bytes(base,offset,tamanio,buffer);
+	uint32_t valorRetorno = 0;
+
+	t_paquete_programa paq_saliente;
+		paq_saliente.id = 'P';
+		paq_saliente.mensaje = orden;
+		paq_saliente.sizeMensaje = strlen(orden);
+	
+	char *paqueteSaliente = serializar_paquete(&paq_saliente, logger);
+
+	bEnv = paq_saliente.tamanio_total;
+	if(sendAll(socket_umv, paqueteSaliente, &bEnv)){
+		log_error(logger, "[PLP] Error en la solicitud de enviar bytes para el segmento de código. Motivo: %s", strerror(errno));
+		free(paqueteSaliente);
+		free(orden);
+		return 0;
+	}
+
+	free(paqueteSaliente);
+	free(orden);
+
+	uint32_t bRec = 0;
+	t_paquete_programa respuesta;
+	log_info(logger, "[PLP] Esperando la respuesta de la UMV.");
+	bRec = recvAll(&respuesta, socket_umv);
+	log_info(logger, "[PLP] La UMV respondió %s", respuesta.mensaje);
+
+	valorRetorno = atoi(respuesta.mensaje);
+	free(respuesta.mensaje);
+
+	return valorRetorno;
+}
+
+char *codificar_enviar_bytes(uint32_t base, uint32_t offset, int tamanio, char *buffer)
+{
+	int offset_codificacion = 0;
+
+	char str_base[10];
+	sprintf(str_base, "%d", base);
+	char str_offset[10];
+	sprintf(str_offset, "%d", offset);
+	char str_tamanio[10];
+	sprintf(str_tamanio, "%d", tamanio);
+	char *comando = "enviar_bytes";
+
+	int base_len = strlen(str_base);
+	int offset_len = strlen(str_offset);
+	int tamanio_len = strlen(str_tamanio);
+	int comando_len = strlen(comando);
+	int buffer_len = strlen(buffer);
+
+	char *orden_completa = calloc(comando_len + 1 + 1 + base_len + 1 + offset_len + 1 + tamanio_len + 1 + buffer_len, 1);
+	memcpy(orden_completa + offset_codificacion, comando, comando_len);
+	offset_codificacion += comando_len;
+	memcpy(orden_completa + offset_codificacion, ",", 1);
+	offset_codificacion += 1;
+
+	memcpy(orden_completa + offset_codificacion, str_base, base_len);
+	offset_codificacion += base_len;
+	memcpy(orden_completa + offset_codificacion, ",", 1);
+	offset_codificacion += 1;
+
+	memcpy(orden_completa + offset_codificacion, str_offset, offset_len);
+	offset_codificacion += offset_len;
+	memcpy(orden_completa + offset_codificacion, ",", 1);
+	offset_codificacion += 1;
+
+	memcpy(orden_completa + offset_codificacion, str_tamanio, tamanio_len);
+	offset_codificacion += tamanio_len;
+	memcpy(orden_completa + offset_codificacion, ",", 1);
+	offset_codificacion += 1;
+
+	memcpy(orden_completa + offset_codificacion, buffer, buffer_len);
+	offset_codificacion += buffer_len;
+	
+	return orden_completa;
 }
 
 char *codificar_crear_segmento(uint32_t id_programa, uint32_t tamanio)
