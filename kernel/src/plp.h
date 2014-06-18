4ab9b4c5f8146b4f3ea32d5253bf39672f0e3d54
hernan-erasmo <socialmetrix@gmail.com>
2014-06-18 15:09:37 -0300

    Se extrajeron las funciones que implementaban la interfaz hacia las operaciones de la UMV y ahora están en un archivo aparte

--- kernel/src/plp.h
+++ kernel/src/plp.h
@@ -7,6 +7,7 @@
 #include <parser/metadata_program.h>
 
 #include "../../utils/comunicacion.h"
+#include "../../utils/interfaz_umv.h"
 #include "kernel.h"
 
 #ifndef PLP_H
@@ -23,15 +24,6 @@ typedef struct datos_plp {
 void *plp(void *puerto_prog);
 
 t_pcb *crearPCB(t_metadata_program *metadata, int id_programa);
-
-uint32_t solicitar_cambiar_proceso_activo(int socket_umv, uint32_t contador_id_programa, t_log *logger);
-uint32_t solicitar_crear_segmento(int socket_umv, uint32_t id_programa, uint32_t tamanio_segmento, t_log *logger);
-uint32_t solicitar_enviar_bytes(int socket_umv, uint32_t base, uint32_t offset, int tamanio, void *buffer, t_log *logger);
-void *solicitar_solicitar_bytes(int socket_umv, uint32_t base, uint32_t offset, int tamanio, t_log *logger);
-char *codificar_cambiar_proceso_activo(uint32_t contador_id_programa);
-char *codificar_crear_segmento(uint32_t id_programa, uint32_t tamanio);
-char *codificar_enviar_bytes(uint32_t base, uint32_t offset, int tamanio, void *buffer, uint32_t *tamanio_de_la_orden_completa);
-char *codificar_solicitar_bytes(uint32_t base, uint32_t offset, int tamanio);
 
 uint32_t crear_segmento_codigo(int socket_umv, t_pcb *pcb, t_paquete_programa *paquete, uint32_t contador_id_programa, t_log *logger);
 uint32_t crear_segmento_stack(int socket_umv, t_pcb *pcb, uint32_t tamanio_stack, uint32_t contador_id_programa, t_log *logger);
---
+++ utils/interfaz_umv.h
@@ -0,0 +1,20 @@
+#include <commons/log.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "comunicacion.h"
+
+#ifndef INTERFAZ_UMV_H
+#define INTERFAZ_UMV_H
+
+uint32_t solicitar_cambiar_proceso_activo(int socket_umv, uint32_t contador_id_programa, t_log *logger);
+uint32_t solicitar_crear_segmento(int socket_umv, uint32_t id_programa, uint32_t tamanio_segmento, t_log *logger);
+uint32_t solicitar_enviar_bytes(int socket_umv, uint32_t base, uint32_t offset, int tamanio, void *buffer, t_log *logger);
+void *solicitar_solicitar_bytes(int socket_umv, uint32_t base, uint32_t offset, int tamanio, t_log *logger);
+char *codificar_cambiar_proceso_activo(uint32_t contador_id_programa);
+char *codificar_crear_segmento(uint32_t id_programa, uint32_t tamanio);
+char *codificar_enviar_bytes(uint32_t base, uint32_t offset, int tamanio, void *buffer, uint32_t *tamanio_de_la_orden_completa);
+char *codificar_solicitar_bytes(uint32_t base, uint32_t offset, int tamanio);
+
+#endif /* INTERFAZ_UMV_H */
